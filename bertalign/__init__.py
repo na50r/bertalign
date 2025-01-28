fcffeb1ec6ed0a1d24d01b4954fa5ab296c1bf95
na50r <shanrosan@gmail.com>
2025-01-28 13:15:50 +0100

    use distiluse-base-multilingual-cased-v2 instead of LaBSE

--- bertalign/__init__.py
+++ bertalign/__init__.py
@@ -10,7 +10,8 @@ from bertalign.encoder import Encoder
 # See other cross-lingual embedding models at
 # https://www.sbert.net/docs/pretrained_models.html
 
-model_name = "LaBSE"
+#model_name = "LaBSE"
+model_name = "distiluse-base-multilingual-cased-v2"
 model = Encoder(model_name)
 
 from bertalign.aligner import Bertalign
